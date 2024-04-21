53e16f39c9af9f4e06c4c1045ab2e1df04df7c27
Adam Eshel <adameshel97@gmail.com>
2024-04-20 20:18:07 -0400

    completes and tests building a graph from an adjacency matrix

--- src/graph_maker.rs
+++ src/graph_maker.rs
@@ -1,14 +1,14 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use petgraph::graph::{DiGraph, NodeIndex};
 
 struct graph_maker(); 
 impl graph_maker {
-    fn parse_adjacency_matrix_from_file_path(&self, file_path: &str) -> Vec<Vec<f32>> {
+     fn parse_adjacency_matrix_from_file_path(&self, file_path: &str) -> Vec<Vec<f32>> {
         let mut adjacency_matrix:  Vec<Vec<f32>> = Vec::new();
         if let Ok(file) = File::open(file_path) {
             let reader = BufReader::new(file);
-            for line in reader.lines() {
-             
+            for line in reader.lines() {             
                     let numbers: Vec<f32> = line
                         .unwrap()
                         .split_whitespace()
@@ -20,12 +20,33 @@ impl graph_maker {
         }
         return adjacency_matrix;
     }
+
+    fn build_graph_from_adjacency_matrix(&self, adjacency_matrix: Vec<Vec<f32>>)  -> DiGraph<i32, f32> {
+        let mut graph: DiGraph<i32, f32> = DiGraph::new();
+        let mut node_indices: Vec<NodeIndex> = Vec::new();
+        for i in 0.. adjacency_matrix.len() {
+            let node_index = graph.add_node(0); 
+            node_indices.push(node_index);
+        }
+        for i in 0.. adjacency_matrix.len() {
+            for j in 0 .. adjacency_matrix[i].len() {
+                if adjacency_matrix[i][j] != 0.0 {
+                    let weight = adjacency_matrix[i][j];
+                    graph.add_edge(node_indices[i], node_indices[j], weight);
+                }
+            }
+        }
+        return graph;
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::graph_maker::graph_maker;
-
+    use crate::graph_maker::DiGraph;
+    use petgraph::graph::NodeIndex;
+    use petgraph::algo::is_isomorphic_matching;
+    
     #[test]
     fn test_adjacency_matrix_from_file() {
         let expected_matrix_1: Vec<Vec<f32>> = vec![
@@ -44,5 +65,26 @@ mod tests {
     let actual_matrix_2 = graph_maker().parse_adjacency_matrix_from_file_path("data/matrix_2.txt");
     assert_eq!(actual_matrix_1, expected_matrix_1);
     assert_eq!(actual_matrix_2, expected_matrix_2);
+    }
+
+    #[test]
+    fn build_graph_from_adjacency_matrix_test() {
+        let input_matrix: Vec<Vec<f32>> = vec![
+        vec![0.1, 0.7, 0.8],
+        vec![0.8, 1.5, 0.0],
+        vec![0.0, 0.8, 0.0],
+    ];
+    let mut expected_graph: DiGraph<i32, f32> = DiGraph::new();
+    let node_indices: Vec<NodeIndex> = input_matrix.iter().map(|_| expected_graph.add_node(0)).collect();
+    expected_graph.add_edge(node_indices[0], node_indices[0], 0.1);
+    expected_graph.add_edge(node_indices[0], node_indices[1], 0.7);
+    expected_graph.add_edge(node_indices[0], node_indices[2], 0.8);
+    expected_graph.add_edge(node_indices[1], node_indices[0], 0.8);
+    expected_graph.add_edge(node_indices[1], node_indices[1], 1.5);
+    expected_graph.add_edge(node_indices[2], node_indices[1], 0.8);
+    let actual_graph = graph_maker().build_graph_from_adjacency_matrix(input_matrix);
+    let node_matcher = |_: &_, _: &_| true;
+    let edge_matcher = |edge1: &f32, edge2: &f32| *edge1 == *edge2;
+    assert!(is_isomorphic_matching(&expected_graph, &actual_graph, node_matcher, edge_matcher));
     }
 }
